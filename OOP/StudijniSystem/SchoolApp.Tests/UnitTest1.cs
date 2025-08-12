afcb35108860415b38728c6b36f8278a34045bdc
Marty808s <martybeats.cz@gmail.com>
2025-08-12 12:17:01 +0200

    project - testing up

--- OOP/StudijniSystem/SchoolApp.Tests/UnitTest1.cs
+++ OOP/StudijniSystem/SchoolApp.Tests/UnitTest1.cs
@@ -3,9 +3,9 @@ dotnet new xunit -n SchoolApp.Tests
 dotnet add SchoolApp.Tests/SchoolApp.Tests.csproj reference path/k/tvemu/hlavnimu/Projektu.csproj
 dotnet test
 
---> jít do závislostí, pøidat referenci na projekt
+--> jít do závislostí, pøidat referenci na projekt --> pøidám dll
 
-spuštìní testu:
+spuštìní testu/pravím na test cs soubor a dát testovat. Následnì ve výstupech pøejít na testy:
 dotnet test
 */
 
