72dff5556deeb2f0a29c4836b79119f386d7152a
Marty808s <martybeats.cz@gmail.com>
2025-09-05 09:19:39 +0200

    data ready na insert

--- DB/ROLAP2/data.py
+++ DB/ROLAP2/data.py
@@ -76,7 +76,7 @@ print(years_hdp)
 
 years_dim = pd.DataFrame({
     "id": range(1,len(years_hdp)+1),
-    "value": years_hdp
+    "nazev": years_hdp
 })
 print(years_dim)
 
@@ -84,14 +84,23 @@ print(years_dim)
 kraje_uzemi = cizinci[["kraj_kod","vuzemi_txt","kraj_txt"]].drop_duplicates() #dropne duplicitní hodnoty na víe sloupcích
 print(kraje_uzemi)
 
-kraje_dim = pd.DataFrame({
+region_dim = pd.DataFrame({
     "id": range(1,len(kraje_uzemi)+1),
-    "kraj_temp_key": kraje_uzemi["kraj_kod"],
-    "name" : kraje_uzemi["kraj_txt"],
+    "source_key": kraje_uzemi["kraj_kod"],
+    "nazev" : kraje_uzemi["kraj_txt"],
     "uzemi" : kraje_uzemi["kraj_txt"]
 })
 
-print(kraje_dim)
+print(region_dim)
+
+country = hdp["Stat"].unique()
+
+country_dim = pd.DataFrame({
+    "id": range(1,len(country)+1),
+    "nazev": country
+})
+
+print(country_dim)
 
 # Tabulky faktů - pomocí merge
 """
@@ -107,10 +116,63 @@ fact_foreigners = foreigners.merge(
 country_map = dict(zip(dim_country["name_cz"], dim_country["country_id"]))
 foreigners["country_id"] = foreigners["country_name"].map(country_map)
 """
-#fact_gdp
+#fact_hdp
+fact_country_hdp = hdp.merge(
+    country_dim,
+    left_on="Stat",
+    right_on="nazev"
+).rename(columns={"id": "fk_country"})
+
+#převod z čirokéfo formátu na dlouhý
+fact_country_hdp_long = fact_country_hdp.melt(
+    id_vars=["fk_country", "Stat"],    # sloupce, které mají zůstat
+    var_name="year",                   # nový název pro původní názvy sloupců (1990,1995,…)
+    value_name="hdp_inhabitant"        # nový název pro hodnoty
+)
+
+fact_country_hdp_long = fact_country_hdp_long.merge(
+    years_dim,
+    left_on="year",
+    right_on="nazev"
+).rename(columns={"id":"fk_year"})
+
+print(fact_country_hdp_long.head(50))
+fact_country_hdp = fact_country_hdp_long[["fk_country", "hdp_inhabitant", "fk_year"]]
+print(fact_country_hdp)
 
 #fact_cizinci
+fact_cizinci = cizinci.merge(
+    country_dim,
+    left_on="stobcan_txt",
+    right_on="nazev"
+).rename(columns={"id": "fk_country",
+                  "hodnota" : "pocet_cizinci",
+                  "idhod" : "source_key"
+              })
 
+fact_cizinci["rok"] = fact_cizinci["rok"].astype(int)
+years_dim["nazev"] = years_dim["nazev"].astype(int)
+
+fact_cizinci = fact_cizinci.merge(
+    years_dim,
+    left_on="rok",
+    right_on="nazev"
+).rename(columns={"id": "fk_year"})
+
+fact_cizinci = fact_cizinci.merge(
+    region_dim[["id","source_key"]],
+    left_on="kraj_kod",
+    right_on="source_key",
+    how="left"
+).rename(columns={"id": "fk_region"})
+
+fact_cizinci = fact_cizinci.rename(columns={"source_key_x" : "source_key"})
+print(fact_cizinci)
+
+fact_cizinci = fact_cizinci[["fk_country","fk_region","pocet_cizinci", "fk_year", "source_key"]]
+print(fact_cizinci)
+
+#fact_cizinci = fact_cizinci[["fk_country", "fk_country"]]
 #Export
 #cizinci.to_csv('./output_cizinci.csv', index=False)
 #hdp.to_csv('./output_hdp.csv', index=False)
