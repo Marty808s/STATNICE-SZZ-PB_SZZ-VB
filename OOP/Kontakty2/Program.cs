cfa4d878cf8c24a4a397406459643c7b83253805
Marty808s <martybeats.cz@gmail.com>
2025-09-07 10:10:15 +0200

    kontakty2 run

---
+++ OOP/Kontakty2/Program.cs
@@ -0,0 +1,70 @@
+﻿//1.Prototype: Pro efektivní vytváření a kopírování kontaktních záznamů. - DONE
+//2. Command: Pro zaznamenávání a možné vrácení změn v kontaktech.
+//3. Iterator: Pro navigaci skrze kolekci kontaktů.
+
+// čas 18 minut
+//--------------------------------------------------
+// Prototype - pro mě kontakt
+public abstract class Prototype
+{
+    public string Jmeno { get; set; }
+    public string Prijmeni { get; set; }
+    public string Telefon { get; set; }
+    public string Email { get; set; }
+
+    public Prototype(string jmeno, string prijmeni, string telefon, string email)
+    {
+        this.Jmeno = jmeno;
+        this.Prijmeni = prijmeni;
+        this.Telefon = telefon;
+        this.Email = email;
+    }
+
+    public Prototype(Prototype other)
+    {
+        this.Jmeno = other.Jmeno;
+        this.Prijmeni = other.Prijmeni;
+        this.Telefon = other.Telefon;
+        this.Email = other.Email;
+    }
+
+    public abstract Prototype Clone();
+}
+
+public class Kontakt : Prototype
+{
+    //konstruktor pro vytvoření
+    public Kontakt(string jmeno, string prijmeni, string telefon, string email)
+        : base(jmeno, prijmeni, telefon, email)
+    {
+    }
+
+    // konstruktor pro kopírování
+    public Kontakt(Kontakt other)
+        : base(other)
+    {
+    }
+
+    public override Prototype Clone()
+    {
+        return new Kontakt(this);
+    }
+}
+
+//--------------------------------------------------
+// Iterator
+
+
+
+//--------------------------------------------------
+//Command
+public interface ICommand
+{
+    void Execute();
+}
+
+//Reaciever - ten spustí logiku - jednotlivé operace
+public class KontaktReciever
+{
+
+}
